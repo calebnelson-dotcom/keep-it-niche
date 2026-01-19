3266c826d5a0525a86070877c3e3dce02cafa78d
grant <grantschmidt8@gmail.com>
2026-01-19 01:34:25 -0600

    create messages

---
+++ public/loader-messages.js
@@ -0,0 +1,23 @@
+// loader-messages.js
+window.LOADER_MESSAGES = [
+  "the gabagool",
+  "i need that ad revenue to hit my account like now",
+  "im making these messages at 4am",
+  "i actually put your suggestions",
+  "who ever put in suggestions to add more movies i hate you",
+  "yall know about trinity?",
+  "go check out aurababy and 55gms",
+  "donate to me please",
+  "shout out to everyone i stole code from",
+  "stop asking for roblox",
+  "i hate past me for being an idiot",
+  "who are the people in germany using ts site",
+  "solace - earl sweatshirt is the insperation for s0lace",
+  "every movie btw",
+  "should i make a site just movies",
+  "go do work bro",
+  "should i make a discord?",
+  "byod is really confusing",
+  "365 movies in 365 days im good so far",
+  "i have a looney tunes bump on my head"
+  ];
